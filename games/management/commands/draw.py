c989f079e79962f855ebbd59ec7d8ff3487faa28
delphython <delphython@gmail.com>
2021-12-15 21:31:22 +0300

    Change algorithm for draw

--- games/management/commands/draw.py
+++ games/management/commands/draw.py
@@ -1,21 +1,24 @@
-from itertools import combinations
+from collections import deque
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from games.models import Game, Santa, Draw, Exclusion
 
 
 def get_pair_exclusion(game):
     exclusions = Exclusion.objects.filter(game=game)
-    return [[exclusion.giver, exclusion.receiver] for exclusion in exclusions]
+    return [(exclusion.giver, exclusion.receiver) for exclusion in exclusions]
 
 
 def make_draw(game):
     santas = Santa.objects.filter(game=game)
 
-    pairs = list(combinations(santas, 2))
-    # filtered_pairs = list(set(pairs) - set(get_pair_exclusion(game)))
+    partners = deque(santas)
+    partners.rotate()
+    pairs = list(zip(santas, partners))
 
-    for giver, receiver in pairs:
+    filtered_pairs = list(set(pairs) - set(get_pair_exclusion(game)))
+
+    for giver, receiver in filtered_pairs:
         draw = Draw.objects.get_or_create(
             game=game,
             giver=giver,
