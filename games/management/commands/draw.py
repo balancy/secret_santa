e3ef93acc80f92b55b352c0ccf79bf1a8e998335
delphython <delphython@gmail.com>
2021-12-16 14:21:46 +0300

    Add pairs rotation function if exclusions exist

--- games/management/commands/draw.py
+++ games/management/commands/draw.py
@@ -11,18 +11,39 @@ def get_pair_exclusion(game):
     return [(exclusion.giver, exclusion.receiver) for exclusion in exclusions]
 
 
-def make_draw(game):
+def make_rotation(pairs, exclusions):
     pairs_amount_to_ignore_exclusion = 4
+
+    for exclusion in exclusions:
+        if (exclusion in pairs) and (len(pairs) > pairs_amount_to_ignore_exclusion):
+            pairs = [list(i) for i in pairs]
+            exclusion = list(exclusion)
+
+            first_rotation_index = pairs.index(exclusion)
+            second_rotation_index = next(i for i, (_, x) in enumerate(pairs)
+                if x == exclusion[0])
+            third_rotation_index = next(i for i, (_, x) in enumerate(pairs)
+                if x == pairs[second_rotation_index][0])
+
+            first_rotation_giver = pairs[first_rotation_index][1]
+            second_rotation_giver = pairs[second_rotation_index][1]
+            third_rotation_giver = pairs[third_rotation_index][1]
+
+            pairs[first_rotation_index][1] = third_rotation_giver
+            pairs[second_rotation_index][1] = first_rotation_giver
+            pairs[third_rotation_index][1] = second_rotation_giver
+
+    return pairs
+
+
+def make_draw(game):
     santas = Santa.objects.filter(games=game)
 
     partners = deque(santas)
     partners.rotate()
     pairs = list(zip(santas, partners))
 
-    modified_pairs = (
-        list(set(pairs) - set(get_pair_exclusion(game)))
-            if len(pairs) > pairs_amount_to_ignore_exclusion else list(set(pairs))
-    )
+    modified_pairs = make_rotation(pairs, get_pair_exclusion(game))
 
     for giver, receiver in modified_pairs:
         draw = Draw.objects.get_or_create(
