00dc1127ec9aae3d5ae0dc5cf4333bddaf095e0b
delphython <delphython@gmail.com>
2021-12-17 12:08:34 +0300

    Add make and send email functions

--- games/management/commands/draw.py
+++ games/management/commands/draw.py
@@ -1,9 +1,50 @@
 import datetime
+import os
+import smtplib
 
 from collections import deque
-
 from django.core.management.base import BaseCommand
+from email.message import EmailMessage
+
 from games.models import Game, Santa, Draw, Exclusion
+
+
+def send_email(smtp_server, subject, to_addr, from_addr, body_text):
+    email_body = (
+        f'From: {from_addr}\r\n' +
+        'To: {to_addr}\r\n' +
+        'Subject: {subject}\r\n{body_text}'
+    )
+    msg = EmailMessage()
+    msg.set_content(body_text)
+    msg["Subject"] = subject
+    msg["From"] = from_addr
+    msg["To"] = to_addr
+
+    smtp_server.send_message(msg)
+
+
+def make_and_send_email_message(game, smtp_server, from_adress):
+    draws = Draw.objects.filter(game=game)
+
+    for draw in draws:
+        subject = f'Жеребьевка в игре {game.name} проведена!'
+        body_text = (
+            f'Жеребьевка в игре {game.name} проведена!\r\n' +
+            'Спешу сообщить кто тебе выпал.\r\n' +
+            'Ваш игрок: {draw.receiver.user.username}\r\n' +
+            'Адрес эл. почты: {draw.receiver.user.email}\r\n' +
+            'Письмо Санте: {draw.receiver.letter_to_santa}\r\n' +
+            'Вишлист: {draw.receiver.wishlist}'
+        )
+
+        send_email(
+            smtp_server,
+            subject,
+            draw.giver.user.email,
+            from_adress,
+            body_text
+        )
 
 
 def get_pair_exclusion(game):
@@ -12,7 +53,7 @@ def get_pair_exclusion(game):
 
 
 def make_rotation(pairs, exclusions):
-    pairs_amount_to_ignore_exclusion = 4
+    pairs_amount_to_ignore_exclusion = 3
 
     for exclusion in exclusions:
         if (exclusion in pairs) and (len(pairs) > pairs_amount_to_ignore_exclusion):
@@ -20,10 +61,14 @@ def make_rotation(pairs, exclusions):
             exclusion = list(exclusion)
 
             first_rotation_index = pairs.index(exclusion)
-            second_rotation_index = next(i for i, (_, x) in enumerate(pairs)
-                if x == exclusion[0])
-            third_rotation_index = next(i for i, (_, x) in enumerate(pairs)
-                if x == pairs[second_rotation_index][0])
+            second_rotation_index = next(
+                i for i, (_, required_element) in enumerate(pairs)
+                if required_element == exclusion[0]
+            )
+            third_rotation_index = next(
+                i for i, (_, required_element) in enumerate(pairs)
+                if required_element == pairs[second_rotation_index][0]
+            )
 
             first_rotation_giver = pairs[first_rotation_index][1]
             second_rotation_giver = pairs[second_rotation_index][1]
@@ -55,8 +100,21 @@ def make_draw(game):
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        from_adress = os.getenv('from_adress')
+        smtp_server = smtplib.SMTP_SSL(
+            os.getenv('smtp_host'),
+            port=os.getenv('smtp_port')
+        )
+        smtp_server.login(
+            os.getenv('mail_server_login'),
+            os.getenv('mail_server_password')
+        )
+
         current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
         games = Game.objects.filter(draw_date=current_date)
         for game in games:
             make_draw(game)
+            make_and_send_email_message(game, smtp_server, from_adress)
+
+        smtp_server.quit()
