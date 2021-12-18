93d10b781825fd901db9d516f1ebdf716bc60ca9
balancy <balancy@gmail.com>
2021-12-18 19:13:02 +0100

    Add arrow images to the index page

--- games/views.py
+++ games/views.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy, reverse
 
