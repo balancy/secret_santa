568961e09fad14fade6421423d1879646202a0aa
delphython <delphython@gmail.com>
2021-12-20 14:38:37 +0300

    Add DEFAULT_FROM_EMAIL setting

--- secret_santa/settings.py
+++ secret_santa/settings.py
@@ -154,6 +154,7 @@ EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 # Wishlists number
 
