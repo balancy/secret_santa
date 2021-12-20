7e04fa55f78b9c0c2349206ee64d3553b7ac5ee2
balancy <balancy@gmail.com>
2021-12-20 12:00:19 +0100

    Make change in deployment script

--- deployment_script.sh
+++ deployment_script.sh
@@ -7,7 +7,7 @@ git pull
 
 echo "Building and lanuching containers"
 sudo docker-compose build
-sudo docker-compose -d up
+sudo docker-compose up -d
 
 echo "Removing unused images and containers"
 docker system prune -f
